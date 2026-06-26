63c1e594245de3aefbb71e2672fca3a5c73fe162
Aryan Rathore <rathorearyan973@gmail.com>
2026-06-26 11:13:05 +0530

    Initial commit or first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//Aryan added this feature or currently working on it
+function existFeatures() {
+    console.log("All this features is already exists in project");
+}
+
+function addNewFeature() {
+    console.log("Aryan added this new features");
+}
+
+function sayGreet() {
+    console.log("Hello name good morning");
+}
+
+
+
